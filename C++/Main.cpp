271e7c9818b14352ac482f3f50833afe076df3d4
Rabindra Khadka <43570954+IamRabindra25@users.noreply.github.com>
2019-02-24 15:56:07 +0100

    Update Main.cpp

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -1,21 +1,6 @@
 
 #include "Implementation.cpp"
 
-<<<<<<< HEAD
-
-
-void template_comparison_test()
-{
-    BSTree<int,int,std::greater<int>> mytree;
-    std::vector<int> treeholder{9,6,8,5,12,15};
-    for(long unsigned int i=0; i< treeholder.size(); i++)
-    {
-        mytree.insert(treeholder[i],treeholder[i]);
-    }
-    std::cout << "inserted treeholder={9,6,8,5,12,15} " << std::endl;
-    std::cout << mytree << std::endl;
-};
-=======
 void template_comparison_test()
 {
     BSTree<int,int,std::greater<int>> Atree;
@@ -94,85 +79,6 @@ void template_comparison_test()
 
 
 
-
-        //Testing balance()
->>>>>>> a9da71cac8a7b4293e7a3e4abd15197e4ff6b0bb
-
-int main()
-{
-    try
-     {
-
-        tree.balance(); //testing  end(),begin(),rebuildBalancedTrees
-        std::cout << tree << std::endl;
-        BSTree<int,int> tree_1{};
-        if(tree_1.begin()==tree.cend())
-        {std::cout << "Right" << std::endl;};  //testing begin()
-
-
-<<<<<<< HEAD
-        //	Testing building-up-tree methods
-        bst.insert(5,10);
-        bst.insert(2,20);
-        bst.insert(7,70);
-        bst.insert(10,100);
-        bst.insert(1,11);
-        bst.insert(3,30);
-        bst.print();
-        bst.find(1);//finding
-        bst.find(3);
-        bst.find(10);
-       	bst.clear();
-
-
-        //  Testing different kind of constructors.
-
-          BSTree<int,int> mybst{};
-          BSTree<char,int> mybst1("TR",2);
-          BSTree<char,int> mybst2{mybst1};
-          std::cout << mybst << std::endl;
-          std::cout << mybst1 << std::endl;
-          std::cout << mybst2 << std::endl;
-          BSTree<char,int>mybst3{std::move(bst1)};
-          std::cout << mybst3 << std::endl;
-          std::cout << mybst1 << std::endl;   //mybst1 will be empty
-
-
-
-
-  //	Testing iterators methods and balance()
- //testing cend() methods, findNearest.
-        int KN=1;
-        if(bst.find(KN)==bst.cend())
-        {std::cout << "Right" << std::endl;};
-
-
-        bst.balance(); //testing  end(),begin(),rebuildBalancedTrees
-        std::cout << bst << std::endl;
-        BSTree<int,int> bst_1{};
-        if(bst_1.begin()==bst.cend())
-        {std::cout << "Right" << std::endl;};  //testing begin()
-
-
-        //	Testing operator=
-
-        bst_1=bst;
-        bst_1.printTree(std::cout);
-        bst.printTree(std::cout);
-        BSTree<int,int> bst_2{};
-        bst_2=std::move(bst_1);
-        bst_2.printTree(std::cout);
-        if(bst_1.begin()==bst.cend()){std::cout << "Right" << std::endl;};
-
-        // 	Testing operator[] and template
-            int key=1;
-            std::cout << mybst[key] << std::endl;
-            template_comparison_test();
-    }
-
-   catch (std::runtime_error& err)
-
-=======
         // Testing operator=
         std::cout << tree[1] << std::endl;
         tree_1=tree;
@@ -183,7 +89,6 @@ int main()
 
    catch (std::runtime_error& err)
 
->>>>>>> a9da71cac8a7b4293e7a3e4abd15197e4ff6b0bb
      {
        std::cerr << err.what() << std::endl;
        return 1;
